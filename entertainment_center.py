84a548d07b123cf114f6164d57355b988d1e9439
Denis Doan <dedoin08@yahoo.de>
2015-08-30 20:30:33 +0100

    Git reflection

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,4 +1,6 @@
+import fresh_tomatoes
 import media
+
 # Den media.py file wird importiert.
 toy_story = media.Movie("Toy Story", "A Story of a boy and his toys that come to life", 
   "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "www.youtube.com/watch?v=4Itwjtj6mwg")
@@ -12,5 +14,17 @@ avatar = media.Movie("Avatar", "A marine on","https://upload.wikimedia.org/wikip
 # avatar and toy_story are instances of the class Movie
 #avatar.show_trailer()
 
-transporter = avatar = media.Movie("Transporter", "An exciting action movie.","https://cdn2.thedissolve.com/articles/1434/detail.bad589e2.jpg", "www.youtube.com/watch?v=0poXFSvX0_4" )
-transporter.show_trailer()
+transporter = media.Movie("Transporter", "An exciting action movie.","https://cdn2.thedissolve.com/articles/1434/detail.bad589e2.jpg", "www.youtube.com/watch?v=0poXFSvX0_4" )
+
+school_of_rock = media.Movie("School of Rock", "Storyline","https://cdn2.thedissolve.com/articles/1434/detail.bad589e2.jpg", "www.youtube.com/watch?v=0poXFSvX0_4" )
+
+ratatouille = media.Movie("Ratatouille", "Storyline","https://cdn2.thedissolve.com/articles/1434/detail.bad589e2.jpg", "www.youtube.com/watch?v=0poXFSvX0_4" )
+
+midnight_in_paris = media.Movie("Midnight in Paris", "Storyline","https://cdn2.thedissolve.com/articles/1434/detail.bad589e2.jpg", "www.youtube.com/watch?v=0poXFSvX0_4" )
+
+hunger_games = media.Movie("Hunger Games", "Storyline","https://cdn2.thedissolve.com/articles/1434/detail.bad589e2.jpg", "www.youtube.com/watch?v=0poXFSvX0_4" )
+
+movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+#fresh_tomatoes.open_movies_page(movies)
+print(media.Movie.valid_ratings)
+
--- media.py
+++ media.py
@@ -1,14 +1,15 @@
 import webbrowser
 
 class Movie():
-  def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube ):
+  valid_ratings = ["G", "PG", "PG-13", "R"]
+   def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube ):
       self.title = movie_title
       self.storyline = movie_storyline
       self.poster_image_url = poster_image
       self.trailer_youtube_url = trailer_youtube
 # These are variables
 # A function will be declared.
-# Self is an argument.
+# Self is an argument, which refers to the object.
   def show_trailer(self):
       webbrowser.open(self.trailer_youtube_url)
   
